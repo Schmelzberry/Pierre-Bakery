d51f516d0c041442d81b46f5c9e58b6dd2565831
Jake Elsberry <jelsberry96@gmail.com>
2023-09-22 09:48:35 -0700

    fix one compiler error. namespace directive issue WIP

---
+++ Pierre.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pierre.Models;
+
+namespace Pierre.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    // Tests will go here!
+  }
+}
---
+++ Pierre.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pierre.Models.Pastry;
+
+namespace Pierre.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    // Tests will go here!
+  }
+}
