acd894497fd9f4172e793a8a7eed5eb4cc7985db
Jake Elsberry <jelsberry96@gmail.com>
2023-09-22 10:04:10 -0700

    pastry test 1 - GREEN phase pass

--- Pierre.Tests/ModelTests/PastryTests.cs
+++ Pierre.Tests/ModelTests/PastryTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Pierre.Models.Pastry;
+using Pierre.Models;
 
 namespace Pierre.Tests
 {
   [TestClass]
   public class PastryTests
   {
-    // Tests will go here!
+
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    { //arrange
+      Pastry testDonut = new Pastry();
+      Assert.AreEqual(typeof(Pastry), testDonut.GetType());
+    }
+
   }
 }
