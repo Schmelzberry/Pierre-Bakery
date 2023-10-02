ea33185a1e019b0a4a06839943a7159b31ccf6e0
Jake Elsberry <jelsberry96@gmail.com>
2023-10-01 17:27:25 -0700

    polish and complete testing coverage

--- Pierre/Models/Bread.cs
+++ Pierre/Models/Bread.cs
@@ -1,30 +1,30 @@
 using System;
 
-namespace Pierre.Models 
+namespace Pierre.Models
 {
-  public class Bread
-  { 
-    
-    public int NumberBread { get; private set; }
-   
-
-  public Bread(int loaves)
-  {
-    NumberBread = loaves;
-  }
-  public int GetPriceBread()
-  {
-    int pricePerLoaf = 5;
-    int discountedPriceBread =0;
-
-    if  (NumberBread > 0)
+    public class Bread
     {
-      int discountedBread = NumberBread / 3;
-      int remainingBread = NumberBread % 3;
+        public int NumberBread { get; private set; }
 
-      discountedPriceBread = (discountedBread * 2 * pricePerLoaf) + (remainingBread * pricePerLoaf);
+        public Bread(int loaves)
+        {
+            NumberBread = loaves;
+        }
+
+        public int GetPriceBread()
+        {
+            int pricePerLoaf = 5;
+            int discountedPriceBread = 0;
+
+            if (NumberBread > 0)
+            {
+                int discountedBread = NumberBread / 3;
+                int remainingBread = NumberBread % 3;
+
+                discountedPriceBread =
+                    (discountedBread * 2 * pricePerLoaf) + (remainingBread * pricePerLoaf);
+            }
+            return discountedPriceBread;
+        }
     }
-    return discountedPriceBread;
-  }
-  }
 }
